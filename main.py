7c32e9cdf606d85a3042cca21faf692a8be216be
Ponjke <krisuly45@gmail.com>
2022-10-20 18:12:33 +0300

    Task

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ size_a = int(raw_a)
 size_b = int(raw_b)
 size_c = int(raw_c)
 
-if size_a == 0 or size_b == 0 or size_c == 0:
-    print('There isnt triangle')
-
-'''
-if perimeter > 20 thеn print(...) elif perimeter < 10 thеn print(...)
-'''
-
 perimeter = size_a + size_b + size_c
 
-if perimeter > 20:
-    print('The biggest size = ' , max(size_a, size_b, size_c))
-elif perimeter < 10:
-    print('The smallest size = ' , min(size_a, size_b, size_c))
+if size_a == 0 or size_b == 0 or size_c == 0:
+    print('There isnt triangle')
+elif size_a > 0 and size_b > 0 and size_c > 0:
+    print('perimeter = ', perimeter)
 
-print('Perimeter = ' , perimeter)
+'''
+if perimeter > 20 thеn print(...) elif perimeter < 10 thеn prin(...)
+'''
+
+if perimeter > 20:
+    print('The biggest size = ', max(size_a, size_b, size_c))
+elif perimeter < 10:
+    print('The smallest size = ', min(size_a, size_b, size_c))
 
 
